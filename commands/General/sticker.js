c6adef1e91111516a75fa681605b0f8af007663d
KatowProject <naufalfaqih443@gmail.com>
2022-03-02 19:54:43 +0700

    removebg

--- commands/General/sticker.js
+++ commands/General/sticker.js
@@ -2,24 +2,25 @@ const WA = require('@open-wa/wa-automate');
 
 exports.run = async (client, message, args) => {
     try {
+        const argstmplt = args.join(' ').includes('--removebg') ? "HQ" : false;
         switch (true) {
             //if regex https and image filetype
             case message.text.includes('https://') && message.text.includes('.png') || message.text.includes('.jpg') || message.text.includes('.jpeg') || message.text.includes('.gif'):
-                client.sendStickerfromUrl(message.chatId, args.join(' '), {}, { author: `KatowProject`, discord: "458342161474387999", pack: `Kato-Sticker`, keepScale: true });
+                client.sendStickerfromUrl(message.chatId, args.join(' '), {}, { author: `KatowProject`, discord: "458342161474387999", pack: `Kato-Sticker`, keepScale: true, removebg: argstmplt });
                 break;
 
             case message.type === 'image':
                 //send image as sticker
                 const den = await WA.decryptMedia(message);
                 const imgbase64 = `data:${message.mimetype};base64,${den.toString('base64')}`;
-                client.sendImageAsSticker(message.chatId, imgbase64, { author: `KatowProject`, discord: "458342161474387999", pack: `Kato-Sticker`, keepScale: true });
+                client.sendImageAsSticker(message.chatId, imgbase64, { author: `KatowProject`, discord: "458342161474387999", pack: `Kato-Sticker`, keepScale: true, removebg: argstmplt });
                 break;
 
             case message.type === 'video':
                 //send video as sticker
                 const den2 = await WA.decryptMedia(message);
                 const vidbase64 = `data:${message.mimetype};base64,${den2.toString('base64')}`;
-                client.sendFile(message.chatId, vidbase64);
+                client.sendImageAsSticker(message.chatId, vidbase64, { author: `KatowProject`, discord: "458342161474387999", pack: `Kato-Sticker`, keepScale: true, removebg: argstmplt });
                 break;
 
             default:
