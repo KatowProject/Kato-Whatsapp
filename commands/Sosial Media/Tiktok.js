6c61e7a4ae78f34b5e4ea03dfa602cc4a9172000
KatowProject <naufalfaqih443@gmail.com>
2022-07-20 07:29:34 +0700

    partial update

--- commands/Sosial Media/Tiktok.js
+++ commands/Sosial Media/Tiktok.js
@@ -8,11 +8,17 @@ exports.run = async (client, message, args) => {
         // get response url =
         if (!query.includes('tiktok.com')) return client.reply(message.from, 'Link invalid!', message.id);
 
-        const response = await axios.get(`https://server1.majhcc.xyz/api/tk?url=${query}`);
+        const response = await axios.get(`https://tiktok-info.p.rapidapi.com/dl/?link=${encodeURIComponent(query)}`, {
+            headers: {
+                'x-rapidapi-host': 'tiktok-info.p.rapidapi.com',
+                'x-rapidapi-key': '7a615ab862mshed830d5c52eeb93p139508jsnd38f34660ac9'
+            }
+        });
+
         const data = response.data;
 
         await client.sendText(message.from, "Kato sedang memproses, tunggu sebentar ya...", message.id);
-        await client.sendFileFromUrl(message.from, data.link, `${Date.now()}.mp4`, `Uncompressed Video ${data.link}`);
+        await client.sendFileFromUrl(message.from, data.videoLinks.download, `${Date.now()}.mp4`, `Uncompressed Video ${data.link}`);
     } catch (err) {
         client.reply(message, `Something went wrong!\n${err}`, message.id);
     }
